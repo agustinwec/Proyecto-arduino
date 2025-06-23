8da48498a5aa3f8e5cdb89b69e803189165938c7
agustinwec <101212599+agustinwec@users.noreply.github.com>
2025-06-23 20:52:19 -0300

    fix(sensor): display initial temperature and humidity reading
    Hice que el programa muestre la temperatura y la humedad solo si hay variación significativa. Introduje la variable 'first_time' para 
    asegurar que la primera lectura se imprima siempre, incluso si no hay diferencias respecto a los valores iniciales.

--- Roadmaps /00 Sensores/temp.ino
+++ Roadmaps /00 Sensores/temp.ino
@@ -1,5 +1,6 @@
 float old_temp = 0;
 float old_hum = 0;
+bool first_time = true; 
 
 
 #include <Arduino_HTS221.h>
@@ -21,10 +22,11 @@ void loop() {
 
   // Verificar si el rango de valores de temperatura es superior a 0,5 ºC
   // y si el rango de valores de humedad es superior al 1 %
-  if (abs(old_temp - temperature_Celcius) >= 0.5 || abs(old_hum - humidity) >= 1)
+  if (first_time || abs(old_temp - temperature_Celcius) >= 0.5 || abs(old_hum - humidity) >= 1)
   { 
     old_temp = temperature_Celcius;
     old_hum = humidity;
+    first_time = false; 
 
   // print each of the sensor values
     Serial.print("Temperature = ");
@@ -38,18 +40,5 @@ void loop() {
     // print an empty line
     Serial.println();
   }
-  
-  Serial.print("Temperature = ");
-  Serial.print(temperature_Celcius);
-  Serial.println(" °C");
-
-  Serial.print("Humidity    = ");
-  Serial.print(humidity);
-  Serial.println(" %");
-
-  // print an empty line
-  Serial.println();
-
-  // wait 1 second to print again
   delay(1000);
 }
